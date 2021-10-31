f6f9c9b936dcae119c8a5ad76906dff637d4212f
alexis aravena <aravena91hotmail.se>
2021-10-31 21:46:57 +0100

    Test for utils test

--- utils/src/main/java/Utils.java
+++ utils/src/main/java/Utils.java
@@ -3,6 +3,11 @@ public class Utils {
     public static String parseUrl(String input){
         return "";
     }
+
+    public static String parseHttpRequestType(String input) {
+        return input.substring(0,input.indexOf(" "));
+    }
+
     public String message(){
         return "Hello from utils!";
     }
--- utils/src/test/java/UtilsTest.java
+++ utils/src/test/java/UtilsTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UtilsTest {
 
     @Test
-    void simpleGetReturnUrl(){
+    public void simpleGetReturnUrl(){
 String url = Utils.parseUrl("""
     GET / HTTP/1.1\r\n \
     Host: www.example.com\r\n \
@@ -14,13 +14,23 @@ String url = Utils.parseUrl("""
     }
 
     @Test
-    void filePathGetReturnsUrl(){
+    public void filePathGetReturnsUrl(){
         String url = Utils.parseUrl("""
                  GET /index.html HTTP/1.1\r\n \
                  Host: www.example.com\r\n \
                  \r\n \
                  """);
         assertThat(url).isEqualTo("/index.html");
+    }
+
+    @Test
+    public void filePathHeadReturnsHEADAndUrl(){
+        String url = Utils.parseHttpRequestType("""
+                 HEAD /index.html HTTP/1.1\r\n \
+                 Host: www.example.com\r\n \
+                 \r\n \
+                 """);
+        assertThat(url).isEqualTo("HEAD");
     }
 
 }
